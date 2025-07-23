b3dd700b5fde23562035c0da42970cd509f5aeea
ytpabus <ytpabuse19@gmail.com>
2025-07-23 17:54:15 +0500

    Update supabase_helpers.py

--- utils/supabase_helpers.py
+++ utils/supabase_helpers.py
@@ -1,7 +1,7 @@
 from supabase import create_client, Client
 
-SUPABASE_URL = "<https://qwtcqnaqhfsjwdnlqyds.supabase.co>"
-SUPABASE_KEY = "<eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3dGNxbmFxaGZzandkbmxxeWRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMxOTAzNTIsImV4cCI6MjA2ODc2NjM1Mn0.yXF1vpaZmUcZWToOw-GccrNYCWuHh2Wa-zieeuk6kUY>"
+SUPABASE_URL = "https://qwtcqnaqhfsjwdnlqyds.supabase.co"
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3dGNxbmFxaGZzandkbmxxeWRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMxOTAzNTIsImV4cCI6MjA2ODc2NjM1Mn0.yXF1vpaZmUcZWToOw-GccrNYCWuHh2Wa-zieeuk6kUY"
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
