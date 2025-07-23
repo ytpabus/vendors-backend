33a89a2c6e054213a433650e6742e24186ccfa35
ytpabus <ytpabuse19@gmail.com>
2025-07-23 17:45:30 +0500

    1

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from supabase_helpers import fetch_all_grouped, upsert_supplier, upsert_vendor, delete_supplier
+from utils.supabase_helpers import fetch_all_grouped, upsert_supplier, upsert_vendor, delete_supplier
 from utils.helpers import CONFIG_PATH, save_field_config
 import json
 import os
